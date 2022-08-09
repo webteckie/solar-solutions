362d0f803cfe56dc73735b00b2f758ed805046f2
Carlos Colon <carlos.colon@optimizely.com>
2022-08-09 14:59:44 -0400

    Cleanup.

--- src/components/UseStateTraining/Welcome.test.tsx
+++ src/components/UseStateTraining/Welcome.test.tsx
@@ -3,7 +3,7 @@ import Welcome from './Welcome'
 
 afterEach(cleanup);
 
-it('should show prop one', async () => {
+it('should show a Welcome title', async () => {
     render(<Welcome title='Hello!' />);
     const propText = screen.getByText(/Hello!/i)
     expect(propText).toBeTruthy()
