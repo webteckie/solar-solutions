321f7c9c8ce6a74ad964b75daa606b6ef52c3a0a
Carlos Colon <carlos.colon@optimizely.com>
2022-08-10 18:21:01 -0400

    Cleanup.

--- src/components/Welcome/__tests__/Welcome.initial.snapshot.test.tsx
+++ src/components/Welcome/__tests__/Welcome.initial.snapshot.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import Welcome from '../Welcome'
 
 afterEach(cleanup);
